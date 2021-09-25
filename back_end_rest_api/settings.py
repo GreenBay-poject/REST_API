4702d73138144f6229a26705b339a52faa3daef4
mashkarharis <mashkarharis@gmail.com>
2021-09-25 17:49:12 +0530

    Update settings.py

--- back_end_rest_api/settings.py
+++ back_end_rest_api/settings.py
@@ -156,7 +156,7 @@ EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'greenbaypid3@gmail.com'
-EMAIL_HOST_PASSWORD = 'supirikollo@3'
+EMAIL_HOST_PASSWORD = 'supirikollo@3supirikollo@3'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
