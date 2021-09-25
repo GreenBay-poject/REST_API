1f7da80d9a165e5e3f5f502a6f71982589d05a92
mashkarharis <mashkarharis@gmail.com>
2021-09-25 17:55:55 +0530

    Update settings.py

--- back_end_rest_api/settings.py
+++ back_end_rest_api/settings.py
@@ -152,11 +152,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #DataFlair
 EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'greenbaypid3@gmail.com'
-EMAIL_HOST_PASSWORD = 'supirikollo@3supirikollo@3'
+EMAIL_HOST_PASSWORD = 'supirikollo@3'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
