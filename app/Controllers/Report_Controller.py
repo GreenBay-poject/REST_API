890701662eb805e6e9f5321e01ef23e893cdfffe
Mohomed Haris Mohomed Ashkar <54017081+mashkarharis@users.noreply.github.com>
2021-11-01 17:33:43 +0530

    Update Report_Controller.py

--- app/Controllers/Report_Controller.py
+++ app/Controllers/Report_Controller.py
@@ -103,7 +103,7 @@ def generate_land_report(request):
         report=reportObject.generate_report()
         print(report)
         # Clear Memory
-        # del reportObject
+        del reportObject
         # Return Report
         gc.collect()
         mem2=memory_usage_psutil()
@@ -140,7 +140,7 @@ def generate_deforestation_report(request):
         report=reportObject.generate_report()
         print(report)
         # Return Report
-        # del reportObject
+        del reportObject
         gc.collect()
         mem2=memory_usage_psutil()
         print("Mem Consume at Begining : "+str(mem1))
